d43e221e5175459efc8a07c2a6fa1dd58b2bfdad
anniket460 <aniketpalase1@gmail.com>
2022-12-02 09:57:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product-list</h1>
+      <button>Normal</button>
+      <Button >Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
