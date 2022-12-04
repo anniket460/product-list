fbd0c7e75f80f2e052a1931d4780f0f8c4960152
anniket460 <aniketpalase1@gmail.com>
2022-12-04 16:25:53 +0530

    React App Created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import {Button} from 'react-bootstrap';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from "./Header";
+import AddProduct from "./AddProduct";
+import ProductList from "./ProductList";
+import TableData from "./TableData";
 
 function App() {
   return (
     <div className="App">
-      <h1>Product-list</h1>
-      <button>Normal</button>
-      <Button >Bootstrap</Button>
+      <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/add" element={<AddProduct />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/" element={<TableData />}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
